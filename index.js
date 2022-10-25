eec59d70fd6104a000174fa411b1d53d3b7d7dab
rakibul58 <rhrahi14@gmail.com>
2022-10-25 16:43:30 +0600

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+
+app.use(cors());
 
 
 const courses = require('./data/courses.json'); 
