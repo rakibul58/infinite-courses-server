a86e8f139d357d74b465a72dba601c5a94fc1f72
rakibul58 <rhrahi14@gmail.com>
2022-10-25 16:03:08 +0600

    courses route added

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+const courses = require('./data/courses.json'); 
+
 app.get('/', (req, res) => {
     res.send('Courses API Running');
 });
+
+
+app.get('/courses' , ( req , res )=>{
+    res.send(courses);
+})
 
 
 app.listen(port, () => {
