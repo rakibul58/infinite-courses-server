ff074e7e97c2fd778068d43c6d2ba0c3b48c466d
rakibul58 <rhrahi14@gmail.com>
2022-10-26 11:49:54 +0600

    new api added

--- index.js
+++ index.js
@@ -16,7 +16,15 @@ app.get('/', (req, res) => {
 
 app.get('/courses' , ( req , res )=>{
     res.send(courses);
-})
+});
+
+app.get('/course/:id' , (req , res)=>{
+
+    const id = req.params.id;
+    const course = courses.find(c => c.id == id);
+    res.send(course);
+
+});
 
 
 app.listen(port, () => {
